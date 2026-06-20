d9651bdbaa41bdc72e78396c03afc48951ef379e
Ruri <ruri@ruri.live>
2026-06-21 03:41:27 +0800

    init

--- backend/app/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .clients import router as clients_router
-from .auth import router as auth_router
-from .oauth2 import router as oauth2_router
-
-__all__ = ["clients_router", "auth_router", "oauth2_router"]
--- start-test-client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/test-client"
-uv sync
-uv run python main.py
